28d6f5f8e4517ccb388ffc47783bb14fbecbbaba
Kelly <rasca0027@gmail.com>
2016-12-11 03:53:15 +0800

    first

---
+++ AdultCrawling/Assets/playerMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class playerMovement : MonoBehaviour {
+	
+
+
+	void Update()
+	{
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+		transform.Translate(x, 0, z);
+
+
+	}
+}
