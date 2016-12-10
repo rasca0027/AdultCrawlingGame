cd8efc3290669544f2b815024d48c1b37a7de69d
han <saikoa0709@gmail.com>
2016-12-10 17:07:41 -0500

    addforce commit

---
+++ AdultCrawling/Assets/addForce.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addForce : MonoBehaviour {
+	public float thrust;
+	void Start () {
+
+	}   
+
+	// Update is called once per frame
+	void Update () {
+
+	}   
+	void OnTriggerEnter() {
+		GameObject.Find ("Player").GetComponent<Rigidbody> ().AddForce (transform.forward * thrust);
+	}   
+
+}
